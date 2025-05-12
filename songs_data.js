a07a09cee8947fb563c6ac365b2040cf14f2a648
mkarklins55 <mkarklins55@gmail.com>
2025-05-12 10:06:24 +0300

    karaoke

---
+++ songs_data.js
@@ -0,0 +1,471 @@
+const songs = [
+  {
+    "title": "The Lost Sock Saga",
+    "audio": "music/The Lost Sock Saga.mp3",
+    "lyrics": "lyrics/The Lost Sock Saga.txt",
+    "image": "cover/The Lost Sock Saga.png",
+    "style": "country"
+  },
+  {
+    "title": "Un mēs braucam",
+    "audio": "music/un mēs braucam.mp3",
+    "lyrics": "lyrics/Un mēs braucam.txt",
+    "image": "cover/Un mēs braucam.png",
+    "style": "metal, melodic, progressive metal, synth, nu metal"
+  },
+  {
+    "title": "There are prisoners trapped between clocks",
+    "audio": "music/There are prisoners trapped between cloc.mp3",
+    "lyrics": "lyrics/There are prisoners trapped between cloc.txt",
+    "image": "cover/There are prisoners trapped between clocks.png",
+    "style": "hip hop, Conscious hip hop, east coast"
+  },
+  {
+    "title": "We are driving",
+    "audio": "music/We are driving past trees.mp3",
+    "lyrics": "lyrics/We are driving.txt",
+    "image": "cover/We are driving.png",
+    "style": "country, country like 1970's"
+  },
+  {
+    "title": "Yesterday I searched for future's perfect",
+    "audio": "music/Yesterday I searched for future's perfec.mp3",
+    "lyrics": "lyrics/Yesterday I searched for future's perfect.txt",
+    "image": "images/Yesterday I searched for future's perfect.jpg",
+    "style": ""
+  },
+  {
+    "title": "Dziesmiņa par puteklīti",
+    "audio": "music/Dziesmina par putekliti.mp3",
+    "lyrics": "lyrics/Dziesmiņa par puteklīti.txt",
+    "image": "images/Dziesmiņa par puteklīti.jpg",
+    "style": ""
+  },
+  {
+    "title": "Whispers fall",
+    "audio": "music/Whispers fall.mp3",
+    "lyrics": "lyrics/Whispers fall.txt",
+    "image": "images/Whispers fall.jpg",
+    "style": ""
+  },
+  {
+    "title": "Putekļi krīt",
+    "audio": "music/Putekļi krīt.mp3",
+    "lyrics": "lyrics/Putekļi krīt.txt",
+    "image": "images/Putekļi krīt.jpg",
+    "style": ""
+  },
+  {
+    "title": "Where roads crumble softly underfoot",
+    "audio": "music/Where roads crumble softly underfoot.mp3",
+    "lyrics": "lyrics/Where roads crumble softly underfoot.txt",
+    "image": "images/Where roads crumble softly underfoot.jpg",
+    "style": ""
+  },
+  {
+    "title": "You swept me like ashes",
+    "audio": "music/You swept me like ashes.mp3",
+    "lyrics": "lyrics/You swept me like ashes.txt",
+    "image": "images/You swept me like ashes.jpg",
+    "style": ""
+  },
+  {
+    "title": "Organic House III",
+    "audio": "music/Organic House III.mp3",
+    "lyrics": "lyrics/Organic House III.txt",
+    "image": "images/Organic House III.jpg",
+    "style": ""
+  },
+  {
+    "title": "Organic House VII",
+    "audio": "music/Organic House VII.mp3",
+    "lyrics": "lyrics/Organic House VII.txt",
+    "image": "images/Organic House VII.jpg",
+    "style": ""
+  },
+  {
+    "title": "Kurzemes_rezidence",
+    "audio": "music/Kurzemes_rezidence.mp3",
+    "lyrics": "lyrics/Kurzemes_rezidence.txt",
+    "image": "images/Kurzemes_rezidence.jpg",
+    "style": ""
+  },
+  {
+    "title": "Wheels are turning, morning's gold",
+    "audio": "music/Wheels are turning, morning's gold.mp3",
+    "lyrics": "lyrics/Wheels are turning, morning's gold.txt",
+    "image": "images/Wheels are turning, morning's gold.jpg",
+    "style": ""
+  },
+  {
+    "title": "Dusty paths wind through the rye",
+    "audio": "music/Dusty paths wind through the rye.mp3",
+    "lyrics": "lyrics/Dusty paths wind through the rye.txt",
+    "image": "images/Dusty paths wind through the rye.jpg",
+    "style": ""
+  },
+  {
+    "title": "Rust on the rails and grass in the ties",
+    "audio": "music/Rust on the rails and grass in the ties.mp3",
+    "lyrics": "lyrics/Rust on the rails and grass in the ties.txt",
+    "image": "images/Rust on the rails and grass in the ties.jpg",
+    "style": ""
+  },
+  {
+    "title": "Riteņi minas pa ceļu, kas nav",
+    "audio": "music/Riteņi minas pa ceļu, kas nav.mp3",
+    "lyrics": "lyrics/Riteņi minas pa ceļu, kas nav.txt",
+    "image": "images/Riteņi minas pa ceļu, kas nav.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ridin' hard on a frozen track",
+    "audio": "music/Ridin' hard on a frozen track.mp3",
+    "lyrics": "lyrics/Ridin' hard on a frozen track.txt",
+    "image": "images/Ridin' hard on a frozen track.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ich spür’ dein Herz, es schlägt so kalt",
+    "audio": "music/Ich spür’ dein Herz, es schlägt so kalt.mp3",
+    "lyrics": "lyrics/Ich spür’ dein Herz, es schlägt so kalt.txt",
+    "image": "images/Ich spür’ dein Herz, es schlägt so kalt.jpg",
+    "style": ""
+  },
+  {
+    "title": "Die letzte Straßenbahn nach Riga",
+    "audio": "music/Die letzte Straßenbahn nach Riga.mp3",
+    "lyrics": "lyrics/Die letzte Straßenbahn nach Riga.txt",
+    "image": "images/Die letzte Straßenbahn nach Riga.jpg",
+    "style": ""
+  },
+  {
+    "title": "Muris",
+    "audio": "music/Muris.mp3",
+    "lyrics": "lyrics/Muris.txt",
+    "image": "images/Muris.jpg",
+    "style": ""
+  },
+  {
+    "title": "Sous mes bottes, la poussière chante",
+    "audio": "music/Sous mes bottes, la poussière chante.mp3",
+    "lyrics": "lyrics/Sous mes bottes, la poussière chante.txt",
+    "image": "images/Sous mes bottes, la poussière chante.jpg",
+    "style": ""
+  },
+  {
+    "title": "In der Ecke liegt der Staub",
+    "audio": "music/In der Ecke liegt der Staub.mp3",
+    "lyrics": "lyrics/In der Ecke liegt der Staub.txt",
+    "image": "images/In der Ecke liegt der Staub.jpg",
+    "style": ""
+  },
+  {
+    "title": "Steel und Blut",
+    "audio": "music/Steel und Blut.mp3",
+    "lyrics": "lyrics/Steel und Blut.txt",
+    "image": "images/Steel und Blut.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ich bin Staub",
+    "audio": "music/Ich bin Staub.mp3",
+    "lyrics": "lyrics/Ich bin Staub.txt",
+    "image": "images/Ich bin Staub.jpg",
+    "style": ""
+  },
+  {
+    "title": "I open my eyes",
+    "audio": "music/I open my eyes.mp3",
+    "lyrics": "lyrics/I open my eyes.txt",
+    "image": "images/I open my eyes.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ma kõnnin tolmus",
+    "audio": "music/Ma kõnnin tolmus.mp3",
+    "lyrics": "lyrics/Ma kõnnin tolmus.txt",
+    "image": "images/Ma kõnnin tolmus.jpg",
+    "style": ""
+  },
+  {
+    "title": "Oh little Button, scared and small",
+    "audio": "music/Oh little Button, scared and small.mp3",
+    "lyrics": "lyrics/Oh little Button, scared and small.txt",
+    "image": "images/Oh little Button, scared and small.jpg",
+    "style": ""
+  },
+  {
+    "title": "Dust in my mouth",
+    "audio": "music/Dust in my mouth.mp3",
+    "lyrics": "lyrics/Dust in my mouth.txt",
+    "image": "images/Dust in my mouth.jpg",
+    "style": ""
+  },
+  {
+    "title": "The Button",
+    "audio": "music/The Button.mp3",
+    "lyrics": "lyrics/The Button.txt",
+    "image": "images/The Button.jpg",
+    "style": ""
+  },
+  {
+    "title": "Dust and Silver",
+    "audio": "music/dust_and_silver.mp3",
+    "lyrics": "lyrics/Dust and Silver.txt",
+    "image": "images/Dust and Silver.jpg",
+    "style": ""
+  },
+  {
+    "title": "Old Man and a Hundred Dreams",
+    "audio": "music/Old Man and a Hundred Dreams.mp3",
+    "lyrics": "lyrics/Old Man and a Hundred Dreams.txt",
+    "image": "images/Old Man and a Hundred Dreams.jpg",
+    "style": ""
+  },
+  {
+    "title": "Sunday Train",
+    "audio": "music/Sunday Train.mp3",
+    "lyrics": "lyrics/Sunday Train.txt",
+    "image": "images/Sunday Train.jpg",
+    "style": ""
+  },
+  {
+    "title": "Два дні їдемо крізь пил",
+    "audio": "music/Два дні їдемо крізь пил.mp3",
+    "lyrics": "lyrics/Два дні їдемо крізь пил.txt",
+    "image": "images/Два дні їдемо крізь пил.jpg",
+    "style": ""
+  },
+  {
+    "title": "Crows return on shattered wings",
+    "audio": "music/Crows return on shattered wings.mp3",
+    "lyrics": "lyrics/Crows return on shattered wings.txt",
+    "image": "images/Crows return on shattered wings.jpg",
+    "style": ""
+  },
+  {
+    "title": "After the autumn sadness",
+    "audio": "music/After the autumn sadness.mp3",
+    "lyrics": "lyrics/After the autumn sadness.txt",
+    "image": "images/After the autumn sadness.jpg",
+    "style": ""
+  },
+  {
+    "title": "We don’t fight wars",
+    "audio": "music/We don’t fight wars.mp3",
+    "lyrics": "lyrics/We don’t fight wars.txt",
+    "image": "images/We don’t fight wars.jpg",
+    "style": ""
+  },
+  {
+    "title": "Lai dzīvo puritānisms",
+    "audio": "music/lai dzīvo puritānisms.mp3",
+    "lyrics": "lyrics/Lai dzīvo puritānisms.txt",
+    "image": "images/Lai dzīvo puritānisms.jpg",
+    "style": ""
+  },
+  {
+    "title": "When all the fucking ropes snap",
+    "audio": "music/When all the fucking ropes snap.mp3",
+    "lyrics": "lyrics/When all the fucking ropes snap.txt",
+    "image": "images/When all the fucking ropes snap.jpg",
+    "style": ""
+  },
+  {
+    "title": "Sanctus, sanctus, Sanctus Algorithmus",
+    "audio": "music/Sanctus, sanctus, Sanctus Algorithmus.mp3",
+    "lyrics": "lyrics/Sanctus, sanctus, Sanctus Algorithmus.txt",
+    "image": "images/Sanctus, sanctus, Sanctus Algorithmus.jpg",
+    "style": ""
+  },
+  {
+    "title": "Smoke in the mirror",
+    "audio": "music/Smoke in the mirror2.mp3",
+    "lyrics": "lyrics/Smoke in the mirror.txt",
+    "image": "images/Smoke in the mirror.jpg",
+    "style": ""
+  },
+  {
+    "title": "The Deal Was Clean",
+    "audio": "music/The Deal Was Clean.mp3",
+    "lyrics": "lyrics/The Deal Was Clean.txt",
+    "image": "images/The Deal Was Clean.jpg",
+    "style": ""
+  },
+  {
+    "title": "I was born",
+    "audio": "music/I was born.mp3",
+    "lyrics": "lyrics/I was born.txt",
+    "image": "images/I was born.jpg",
+    "style": ""
+  },
+  {
+    "title": "He loved the burn",
+    "audio": "music/He loved the burn.mp3",
+    "lyrics": "lyrics/He loved the burn.txt",
+    "image": "images/He loved the burn.jpg",
+    "style": ""
+  },
+  {
+    "title": "Out by the edge of the pinewood trail",
+    "audio": "music/Out by the edge of the pinewood trail.mp3",
+    "lyrics": "lyrics/Out by the edge of the pinewood trail.txt",
+    "image": "images/Out by the edge of the pinewood trail.jpg",
+    "style": ""
+  },
+  {
+    "title": "I should sort through my papers",
+    "audio": "music/I should sort through my papers.mp3",
+    "lyrics": "lyrics/I should sort through my papers.txt",
+    "image": "images/I should sort through my papers.jpg",
+    "style": ""
+  },
+  {
+    "title": "Laika ziņas",
+    "audio": "music/Radio teic laika ziņas.mp3",
+    "lyrics": "lyrics/Laika ziņas.txt",
+    "image": "images/Laika ziņas.jpg",
+    "style": ""
+  },
+  {
+    "title": "A Little Song for Joy",
+    "audio": "music/A Little Song for Joy.mp3",
+    "lyrics": "lyrics/A Little Song for Joy.txt",
+    "image": "images/A Little Song for Joy.jpg",
+    "style": ""
+  },
+  {
+    "title": "Broken Chain Anthem",
+    "audio": "music/Broken Chain Anthem.mp3",
+    "lyrics": "lyrics/Broken Chain Anthem.txt",
+    "image": "images/Broken Chain Anthem.jpg",
+    "style": ""
+  },
+  {
+    "title": "Some fucking nature scene",
+    "audio": "music/Some fucking nature scene.mp3",
+    "lyrics": "lyrics/Some fucking nature scene.txt",
+    "image": "images/Some fucking nature scene.jpg",
+    "style": ""
+  },
+  {
+    "title": "A Hundred Kilometres",
+    "audio": "music/I started rollin’ early, sun just touchi.mp3",
+    "lyrics": "lyrics/A Hundred Kilometres.txt",
+    "image": "images/A Hundred Kilometres.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ich zähle jeden Atemzug",
+    "audio": "music/Ich zähle jeden Atemzug.mp3",
+    "lyrics": "lyrics/Ich zähle jeden Atemzug.txt",
+    "image": "images/Ich zähle jeden Atemzug.jpg",
+    "style": ""
+  },
+  {
+    "title": "One Last Time (Burn It Down)",
+    "audio": "music/Wrong shoes, wrong night.mp3",
+    "lyrics": "lyrics/One Last Time (Burn It Down).txt",
+    "image": "images/One Last Time (Burn It Down).jpg",
+    "style": ""
+  },
+  {
+    "title": "The Old Cat Walks",
+    "audio": "music/Red dusk falls on his tired face.mp3",
+    "lyrics": "lyrics/The Old Cat Walks.txt",
+    "image": "images/The Old Cat Walks.jpg",
+    "style": ""
+  },
+  {
+    "title": "Awake to Be Happy",
+    "audio": "music/You lit me up, then walked away.mp3",
+    "lyrics": "lyrics/Awake to Be Happy.txt",
+    "image": "images/Awake to Be Happy.jpg",
+    "style": ""
+  },
+  {
+    "title": "You fucked, you ran, you left the light",
+    "audio": "music/You fucked, you ran, you left the light.mp3",
+    "lyrics": "lyrics/You fucked, you ran, you left the light.txt",
+    "image": "images/You fucked, you ran, you left the light.jpg",
+    "style": ""
+  },
+  {
+    "title": "I’m the Fuckin’ Cat",
+    "audio": "music/I don’t knead, I don’t purr, I don’t play.mp3",
+    "lyrics": "lyrics/I’m the Fuckin’ Cat.txt",
+    "image": "images/I’m the Fuckin’ Cat.jpg",
+    "style": ""
+  },
+  {
+    "title": "Ash and Velvet",
+    "audio": "music/She stitched my name in a coat of smoke.mp3",
+    "lyrics": "lyrics/Ash and Velvet.txt",
+    "image": "images/Ash and Velvet.jpg",
+    "style": ""
+  },
+  {
+    "title": "Vēja zirdziņš",
+    "audio": "music/Dipadu dapadu.mp3",
+    "lyrics": "lyrics/Vēja zirdziņš.txt",
+    "image": "images/Vēja zirdziņš.jpg",
+    "style": ""
+  },
+  {
+    "title": "Pūpoliņi",
+    "audio": "music/Pūpoliņi, pūpoliņi.mp3",
+    "lyrics": "lyrics/Pūpoliņi.txt",
+    "image": "images/Pūpoliņi.jpg",
+    "style": ""
+  },
+  {
+    "title": "AND THEN THEY SCREAM",
+    "audio": "music/first row freaks.mp3",
+    "lyrics": "lyrics/AND THEN THEY SCREAM.txt",
+    "image": "images/AND THEN THEY SCREAM.jpg",
+    "style": ""
+  },
+  {
+    "title": "If Someone Licks the Stamp",
+    "audio": "music/We got your letter.mp3",
+    "lyrics": "lyrics/If Someone Licks the Stamp.txt",
+    "image": "images/If Someone Licks the Stamp.jpg",
+    "style": ""
+  },
+  {
+    "title": "Four Days in Paris",
+    "audio": "music/Had a little money from the cow we sold.mp3",
+    "lyrics": "lyrics/Four Days in Paris.txt",
+    "image": "images/Four Days in Paris.jpg",
+    "style": ""
+  },
+  {
+    "title": "WALK, MOTHERFUCKER",
+    "audio": "music/Streets don’t care.mp3",
+    "lyrics": "lyrics/WALK, MOTHERFUCKER.txt",
+    "image": "images/WALK, MOTHERFUCKER.jpg",
+    "style": ""
+  },
+  {
+    "title": "Skolas diena",
+    "audio": "music/Katru rītu agri ceļos.mp3",
+    "lyrics": "lyrics/Skolas diena.txt",
+    "image": "images/Skolas diena.jpg",
+    "style": ""
+  },
+  {
+    "title": "Klucis un Cena",
+    "audio": "music/Alfrēds Klucis — darbā sētnieks.mp3",
+    "lyrics": "lyrics/Klucis un Cena.txt",
+    "image": "images/Klucis un Cena.jpg",
+    "style": ""
+  },
+  {
+    "title": "Sidewalk of Shards",
+    "audio": "music/Shiny shards in sidewalk seams.mp3",
+    "lyrics": "lyrics/Sidewalk of Shards.txt",
+    "image": "images/Sidewalk of Shards.jpg",
+    "style": ""
+  }
+];
