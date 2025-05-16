d887dcf627b7ff39b135954ea3d8fdd14f9a82d7
mkarklins55 <mkarklins55@gmail.com>
2025-05-16 11:18:07 +0300

    burunduki

--- songs_data.js
+++ songs_data.js
@@ -479,7 +479,7 @@ const songs = [
     "title": "Bur man laimi",
     "audio": "music/Bur man laimi.mp3",
     "lyrics": "lyrics/Bur man laimi.txt",
-    "image": "cover/Bur man laimi.png",
+    "image": "cover/burunduki_eurovision.png",
     "style": "darkwave, erotic darkwave ballad, sleazy synth noir, melodic electro-decadence, industrial crooner pop, gothic bedroom glam, dirty love song for broken neon, cinematic fetish pop, folk polyphony, ritual chant-pop, Baltic ethno-electro, female vocal harmony, trance-folk minimalism, shamanic drum pulse, nature-inspired avant-pop, contemporary pagan fusion, ancestral vocal layering, ceremonial vocal trance"
   }
 ];
