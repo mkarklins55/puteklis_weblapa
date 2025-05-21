64732cd85cb676badcb3688e20ad244252db7607
mkarklins55 <mkarklins55@gmail.com>
2025-05-21 22:15:01 +0300

    desmit_kukainisi

--- songs_data.js
+++ songs_data.js
@@ -559,5 +559,12 @@ const songs = [
     "lyrics": "lyrics/Es saviju vainadziņu, līgo, Jānīti.txt",
     "image": "cover/vainagu_deja.png",
     "style": "darkwave, erotic darkwave ballad, sleazy synth noir, melodic electro-decadence, industrial crooner pop, gothic bedroom glam, dirty love song for broken neon, cinematic fetish pop, folk polyphony, ritual chant-pop, Baltic ethno-electro, female vocal harmony"
+  },
+            {
+    "title": "Desmit mazi kukainīši",
+    "audio": "music/Desmit mazi kukainīši ceļoja ar plostu.mp3",
+    "lyrics": "lyrics/Desmit mazi kukainīši ceļoja ar plostu.txt",
+    "image": "cover/desmit_kukainisi.png",
+    "style": "darkwave, erotic darkwave ballad, sleazy synth noir, melodic electro-decadence, industrial crooner pop, gothic bedroom glam, dirty love song for broken neon, cinematic fetish pop, folk polyphony, ritual chant-pop, Baltic ethno-electro, female vocal harmony"
   }
 ];
