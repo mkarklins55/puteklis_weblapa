adecdf1290acd95ceedd6f53a7175e06b7ba5853
mkarklins55 <mkarklins55@gmail.com>
2026-09-24 08:40:31 +0300

    Remove local TLS private key from published site

--- http_server.py
+++ http_server.py
@@ -1,16 +1,19 @@
 import http.server
 import ssl
+from pathlib import Path
 
 server_address = ('0.0.0.0', 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 
 # Izveido SSL kontekstu
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+context.load_cert_chain(
+    certfile=Path(__file__).with_name('cert.pem'),
+    keyfile=Path.home() / '.config' / 'puteklis' / 'key.pem',
+)
 
 # Tagad 'ietinam' socket ar SSL, bet izmantojam kontekstu
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print("Serveris darbojas uz https://localhost:4443")
 httpd.serve_forever()
-
